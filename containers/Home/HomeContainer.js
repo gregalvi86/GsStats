941bb42af1c0a38a23257c425dcce7fd7137b387
gregalvi86 <53024417+gregalvi86@users.noreply.github.com>
2023-11-19 22:23:32 -0600

    Update HomeContainer.js

--- containers/Home/HomeContainer.js
+++ containers/Home/HomeContainer.js
@@ -5,7 +5,7 @@ class HomeContainer extends Component {
   componentDidMount() {
     document.title = 'uxscoreboard'
     document.getElementsByTagName('meta')['description'].content =
-      'uxscoreboard · A sports scoreboard web app built on ES6, React, and Node.js—features MLB, NBA, NFL, NHL, and MLS (coming soon) games.'
+      'gssportscentral · A sports scoreboard web app built on ES6, React, and Node.js—features MLB, NBA, NFL, NHL, and MLS (coming soon) games.'
   }
   render() {
     return <Home />
